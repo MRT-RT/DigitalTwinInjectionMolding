2eb60956359a586c3488402bd95d4f7aec23f530
SlothFox <alexander.rehmer@gmail.com>
2022-01-17 15:19:39 +0100

    ResultVisual

--- IdentQualityModels_Durchmesser_LSTM.py
+++ IdentQualityModels_Durchmesser_LSTM.py
@@ -67,104 +67,10 @@ if __name__ == '__main__':
     
 
     Modellierungsplan = pkl.load(open('Modellierungsplan.pkl','rb'))
-    counter = range(1,14)
+    counter = [7,10,11,12,13]
     
     multiprocessing.freeze_support()
     
     pool = multiprocessing.Pool()
     
     result = pool.starmap(Fit_LSTM_to_Charges, zip(Modellierungsplan,counter) ) 
-
-
-
-# idx_min = results_LSTM['loss_val'].idxmin()
-
-# param = results_LSTM.loc[idx_min]['params']
-
-# quality_model.AssignParameters(param)
-
-
-# # Evaluate model on training and validation data
-# y_val = []
-# e_val = []
-# y_val_hist = []
-# c_val_hist = []
-
-# for i in range(0,len(data['y_val'])): 
-#     c,y = quality_model.Simulation(data['init_state_val'][i], data['u_val'][i],None,data['switch_val'][i])
-    
-#     c = np.array(c)
-#     y = np.array(y)
-    
-#     y_val_hist.append(y)
-#     c_val_hist.append(c)
-    
-#     y_val.append(y[-1][0])
-#     e_val.append(data['y_val'][i]-y_val[-1])
-
-
-# y_true = np.array(data['y_val']).reshape((-1,1))
-# y_val = np.array(y_val).reshape((-1,1))
-# e_val = np.array(e_val).reshape((-1,1))
-# cycles_val_label = np.array(cycles_val_label).reshape((-1,))
-# charge_val_label = np.array(charge_val_label).reshape((-1,1))
-
-# results_val = pd.DataFrame(data=np.hstack([y_true,y_val,e_val,
-#                              charge_val_label]),
-#                            index = cycles_val_label,
-#                        columns=['y_true','y_est','e','charge'])
-
-# # y_train = []
-# y_train = []
-# e_train = []
-# y_train_hist = []
-# c_train_hist = []
-
-# for i in range(0,len(data['y_train'])): 
-#     c,y = quality_model.Simulation(data['init_state_train'][i], data['u_train'][i],None,data['switch_train'][i])
-    
-#     c = np.array(c)
-#     y = np.array(y)
-    
-#     y_train_hist.append(y)
-#     c_train_hist.append(c)
-    
-#     y_train.append(y[-1][0])
-#     e_train.append(data['y_train'][i]-y_train[-1])
-
-
-# y_true = np.array(data['y_train']).reshape((-1,1))
-# y_train = np.array(y_train).reshape((-1,1))
-# e_train = np.array(e_train).reshape((-1,1))
-# cycles_train_label = np.array(cycles_train_label,dtype=int).reshape((-1,))
-# charge_train_label = np.array(charge_train_label,dtype=int).reshape((-1,1))
-
-# results_train = pd.DataFrame(data=np.hstack([y_true,y_train,e_train,
-#                              charge_train_label]),
-#                              index = cycles_train_label,
-#                              columns=['y_true','y_est','e','charge'])
-
-# # Plot results
-# plt.figure()
-# plt.plot(np.array(results_train['y_true']),results_train['y_est'],'o')
-
-# plt.figure()
-# plt.plot(np.array(results_train['y_true']),results_train['e'],'o')
-
-# plt.figure()
-# plt.plot(np.array(results_train.index),results_train['y_true'],'o')
-# plt.plot(np.array(results_train.index),results_train['y_est'],'d')
-# plt.plot(np.array(results_val.index),results_val['y_true'],'o')
-# plt.plot(np.array(results_val.index),results_val['y_est'],'d')
-
-# plt.figure()
-# sns.stripplot(x="charge", y="e", data=results_train,
-#               size=4, color=".3", linewidth=0)
-# sns.stripplot(x="charge", y="e", data=results_val,
-#               size=4,  linewidth=0)
-
-# plt.figure()
-# plt.plot(results_train.index,results_train['e'],'d')
-
-
-#
