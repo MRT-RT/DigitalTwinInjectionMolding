a6753f05dbda3699704c66cc8d41faca526243a7
SlothFox <alexander.rehmer@gmail.com>
2022-01-19 16:54:07 +0100

    Pres

--- IdentQualityModels_Durchmesser_GRU.py
+++ IdentQualityModels_Durchmesser_GRU.py
@@ -23,16 +23,24 @@ from DIM.miscellaneous.PreProcessing import LoadData
     
 def Fit_GRU_to_Charges(charges,counter):
     
+    path = 'Results/GRU_2c_1sub_5in_Plan_c1_c14/'
     dim_c = 2
     
+    u_inj_lab= ['p_wkz_ist','T_wkz_ist' ,'p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    u_press_lab = ['p_wkz_ist','T_wkz_ist','p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    u_cool_lab = ['p_wkz_ist','T_wkz_ist','p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    
+    u_lab = [u_inj_lab,u_press_lab,u_cool_lab]
+    
+    y_lab = ['Durchmesser_innen']
+    
     data,cycles_train_label,cycles_val_label,charge_train_label,charge_val_label = \
-    LoadData(dim_c,charges)
+    LoadData(dim_c,charges,y_lab,u_lab)
     
-    injection_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='inject')
-    press_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='press')
-    cool_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='cool')
+    one_model = GRU(dim_u=5,dim_c=dim_c,dim_hidden=10,dim_out=1,name='inject')
+
     
-    for rnn in [injection_model,press_model,cool_model]:
+    for rnn in [one_model]:
         name = rnn.name
         
         initial_params = {'b_r_'+name: np.random.uniform(-2,0,(dim_c,1)),
@@ -41,7 +49,7 @@ def Fit_GRU_to_Charges(charges,counter):
         
         rnn.InitialParameters = initial_params
         
-    quality_model = QualityModel(subsystems=[injection_model,press_model,cool_model],
+    quality_model = QualityModel(subsystems=[one_model],
                                   name='q_model_Durchmesser_innen')
     
     
--- IdentQualityModels_Durchmesser_LSTM.py
+++ IdentQualityModels_Durchmesser_LSTM.py
@@ -22,17 +22,24 @@ from DIM.miscellaneous.PreProcessing import LoadData
    
 def Fit_LSTM_to_Charges(charges,counter):
     
-    path = 'Results/LSTM_2c_1sub_2in_allCharg/'
+    path = 'Results/LSTM_2c_1sub_2in_Plan_c1_c14/'
     dim_c = 2
     
+    u_inj_lab= ['p_wkz_ist','T_wkz_ist']# ,'p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    u_press_lab = ['p_wkz_ist','T_wkz_ist']#,'p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    u_cool_lab = ['p_wkz_ist','T_wkz_ist']#,'p_inj_ist','Q_Vol_ist','V_Screw_ist']
+    
+    u_lab = [u_inj_lab,u_press_lab,u_cool_lab]
+    
+    y_lab = ['Durchmesser_innen']
+    
     data,cycles_train_label,cycles_val_label,charge_train_label,charge_val_label = \
-    LoadData(dim_c,charges)
+    LoadData(dim_c,charges,y_lab,u_lab)
     
-    injection_model = LSTM(dim_u=5,dim_c=dim_c,dim_hidden=10,dim_out=1,name='inject')
-    press_model = LSTM(dim_u=5,dim_c=dim_c,dim_hidden=10,dim_out=1,name='press')
-    cool_model = LSTM(dim_u=5,dim_c=dim_c,dim_hidden=10,dim_out=1,name='cool')
+    one_model = LSTM(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='inject')
+
     
-    for rnn in [injection_model,press_model,cool_model]:
+    for rnn in [one_model]:
         name = rnn.name
         
         initial_params = {'b_f_'+name: np.random.uniform(0,1,(dim_c,1)),
@@ -41,7 +48,7 @@ def Fit_LSTM_to_Charges(charges,counter):
         
         rnn.InitialParameters = initial_params
         
-    quality_model = QualityModel(subsystems=[injection_model,press_model,cool_model],
+    quality_model = QualityModel(subsystems=[one_model],
                                   name='q_model_Durchmesser_innen')
     
     
@@ -49,13 +56,14 @@ def Fit_LSTM_to_Charges(charges,counter):
               "print_level":2}
     
     
-    results_LSTM = ModelTraining(quality_model,data,initializations=50, BFR=False, 
+    results_LSTM = ModelTraining(quality_model,data,initializations=20, BFR=False, 
                       p_opts=None, s_opts=s_opts)
     
     results_LSTM['Chargen'] = 'c'+str(counter)
     
     pkl.dump(results_LSTM,open(path+'LSTM_Durchmesser_innen_c'+str(counter)+'.pkl','wb'))
-
+    
+    print('Finished charge ' +  str(counter))
     return results_LSTM  
 
 
@@ -63,15 +71,9 @@ def Fit_LSTM_to_Charges(charges,counter):
 if __name__ == '__main__':
     
     print('Process started..')
-    
-    u_inj_lab= ['p_wkz_ist','T_wkz_ist' ,'p_inj_ist','Q_Vol_ist','V_Screw_ist']
-    u_press_lab = ['p_wkz_ist','T_wkz_ist','p_inj_ist','Q_Vol_ist','V_Screw_ist']
-    u_cool_lab = ['p_wkz_ist','T_wkz_ist','p_inj_ist','Q_Vol_ist','V_Screw_ist']    
-
-    y_lab = ['Durchmesser_innen']
-        
+            
     Modellierungsplan = pkl.load(open('Modellierungsplan.pkl','rb'))
-    counter = [411]
+    counter = range(1,14)
     
     multiprocessing.freeze_support()
     
