b0b4100bebf30a352f031ecd02c0d8fa9dd38d19
SlothFox <alexander.rehmer@gmail.com>
2022-02-16 09:20:49 +0100

    Update GRU_2c_3sub_2in_tuned.py

--- Results/GRU_2c_3sub_2in_all_init/GRU_2c_3sub_2in_tuned.py
+++ Results/GRU_2c_3sub_2in_all_init/GRU_2c_3sub_2in_tuned.py
@@ -32,8 +32,8 @@ def Fit_GRU(counter,initial_params=None):
     split = 'all'
     # split = 'part'
     
-    # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-    path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     
     u_inj= ['p_wkz_ist','T_wkz_ist']
@@ -64,17 +64,15 @@ def Fit_GRU(counter,initial_params=None):
     quality_model = QualityModel(subsystems=[inj_model,press_model,cool_model],
                                   name='q_model')
     
-    # s_opts = {"hessian_approximation": 'limited-memory',"max_iter": 1000,
-    #           "print_level":5}
+   
+    s_opts = {"max_iter": 100}
     
-    s_opts = None
-    
-    results_GRU = ParallelModelTraining(quality_model,data,initializations=2, BFR=False, 
+    results_GRU = ParallelModelTraining(quality_model,data,initializations=1, BFR=False, 
                       p_opts=None, s_opts=s_opts)
     
     results_GRU['Chargen'] = 'c'+str(counter)
     
-    pkl.dump(results_GRU,open('GRU_Durchmesser_innen_c'+str(counter)+'_tuned.pkl','wb'))
+    pkl.dump(results_GRU,open('GRU_Durchmesser_innen_c'+str(counter)+'_tuned_full.pkl','wb'))
     
     print('Charge '+str(counter)+' finished.')
     
@@ -84,9 +82,9 @@ def Fit_GRU(counter,initial_params=None):
 res = pkl.load(open('GRU_Durchmesser_innen_c0_init.pkl','rb'))
 res_sorted = res.sort_values('loss_val')
 
-initial_params = [res_sorted.iloc[i]['params'] for i in range(0,1)]
+initial_params = [res_sorted.iloc[i]['params'] for i in range(0,10)]
 
-for i in range(0,1):
+for i in range(0,10):
     
     Fit_GRU(i,initial_params[i])
     
