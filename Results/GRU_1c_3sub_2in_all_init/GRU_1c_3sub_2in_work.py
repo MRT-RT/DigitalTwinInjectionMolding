cff34a98860979344ccf55618ea9615e5475f333
SlothFox <alexander.rehmer@gmail.com>
2022-02-18 13:11:20 +0100

    work

--- Results/GRU_1c_3sub_2in_all_init/GRU_1c_3sub_2in_work.py
+++ Results/GRU_1c_3sub_2in_all_init/GRU_1c_3sub_2in_work.py
@@ -19,7 +19,7 @@ from DIM.optim.param_optim import ParallelModelTraining, ModelTraining
 from DIM.miscellaneous.PreProcessing import LoadDynamicData
 
 ''' Data Loading '''  
-charges = list(range(1,2))
+charges = list(range(1,275))
 dim_c = 1
 
 # split = 'all'
@@ -27,8 +27,8 @@ split = 'part'
 
 # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
 # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-# path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-path = 'C:/Users/LocalAdmin/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+# path = 'C:/Users/LocalAdmin/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
 
 u_inj= ['p_wkz_ist','T_wkz_ist']
 u_press= ['p_wkz_ist','T_wkz_ist']
@@ -64,7 +64,7 @@ quality_model = QualityModel(subsystems=[inj_model,press_model,cool_model],
                               name='q_model')
     
 ''' Optimize injection model '''
-s_opts = {"max_iter": 5,'step':1}
+s_opts = {"max_iter": 50,'step':1}
     
 res1 = ModelTraining(quality_model,data,initializations=1, BFR=False, 
                   p_opts=None, s_opts=s_opts)
@@ -86,7 +86,7 @@ cool_model.InitialParameters = {p:res1.iloc[0]['params'][p] for p in cool_model.
 quality_model = QualityModel(subsystems=[inj_model,press_model,cool_model],
                               name='q_model')
     
-s_opts = {"max_iter": 5,'step':1}
+s_opts = {"max_iter": 50,'step':1}
     
 res2 = ModelTraining(quality_model,data,initializations=1, BFR=False, 
                   p_opts=None, s_opts=s_opts)
@@ -97,16 +97,16 @@ press_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=1,dim_out=1,name='press')
 cool_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='cool')
     
 
-inj_model.InitialParameters = {p:res1.iloc[0]['params'][p] for p in inj_model.Parameters.keys()}
-press_model.InitialParameters = {p:res1.iloc[0]['params'][p] for p in press_model.Parameters.keys()}
-cool_model.InitialParameters = {p:res1.iloc[0]['params'][p] for p in cool_model.Parameters.keys()}
+inj_model.InitialParameters = {p:res2.iloc[0]['params'][p] for p in inj_model.Parameters.keys()}
+press_model.InitialParameters = {p:res2.iloc[0]['params'][p] for p in press_model.Parameters.keys()}
+cool_model.InitialParameters = {p:res2.iloc[0]['params'][p] for p in cool_model.Parameters.keys()}
 cool_model.InitialParameters['b_z_cool']=-2*np.ones((1,1))
 
 
 quality_model = QualityModel(subsystems=[inj_model,press_model,cool_model],
                               name='q_model')
     
-s_opts = {"max_iter": 5,'step':1}
+s_opts = {"max_iter": 50,'step':1}
     
 res3 = ModelTraining(quality_model,data,initializations=1, BFR=False, 
                   p_opts=None, s_opts=s_opts)
