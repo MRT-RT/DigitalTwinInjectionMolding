c8b340e1f263074c1a0ceab35ede776b733746b7
SlothFox <alexander.rehmer@gmail.com>
2022-02-21 13:23:08 +0100

    1sub

--- Results/GRU_CCTA/GRU_1sub_2in1.py
+++ Results/GRU_CCTA/GRU_1sub_2in1.py
@@ -34,8 +34,12 @@ def Fit_GRU(dim_c,initial_params=None):
     path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-       
-    u_lab = [['p_wkz_ist','T_wkz_ist']]
+    
+    u_inj= ['p_wkz_ist','T_wkz_ist']
+    u_press= ['p_wkz_ist','T_wkz_ist']
+    u_cool= ['p_wkz_ist','T_wkz_ist']
+    
+    u_lab = [u_inj,u_press,u_cool]
     y_lab = ['Durchmesser_innen']
     
     data,cycles_train_label,cycles_val_label,charge_train_label,charge_val_label = \
@@ -48,20 +52,25 @@ def Fit_GRU(dim_c,initial_params=None):
     data['init_state_val'] = c0_val
     
     
-    one_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='one')
+    inj_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=1,dim_out=1,name='inj')
+    press_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=1,dim_out=1,name='press')
+    cool_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,dim_out=1,name='cool')
     
-    one_model.InitialParameters = {'b_z_one':np.random.uniform(-100,-2,(dim_c,1))}
-   
-    quality_model = QualityModel(subsystems=[one_model],
+    press_model.InitialParameters = {'b_z_press':np.random.uniform(-100,-2,(dim_c,1))}
+    cool_model.InitialParameters = {'b_z_cool':np.random.uniform(-100,-2,(dim_c,1))}        
+
+        
+    quality_model = QualityModel(subsystems=[inj_model,press_model,cool_model],
                                   name='q_model')
     
     
     s_opts = {"max_iter": 200, 'step':0.1}
+    # s_opts = None
     
     results_GRU = ParallelModelTraining(quality_model,data,initializations=10, BFR=False, 
                       p_opts=None, s_opts=s_opts)
         
-    pkl.dump(results_GRU,open('GRU_c'+str(dim_c)+'_1sub.pkl','wb'))
+    pkl.dump(results_GRU,open('GRU_c'+str(dim_c)+'.pkl','wb'))
     
 
     
