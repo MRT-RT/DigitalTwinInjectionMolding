9169f0ccc1b590448f9596b9dd87dcc2f106469a
SlothFox <alexander.rehmer@gmail.com>
2022-02-21 14:17:30 +0100

    update

--- Results/GRU_CCTA/GRU_1sub_2in.py
+++ Results/GRU_CCTA/GRU_1sub_2in.py
@@ -58,8 +58,8 @@ def Fit_GRU(dim_c,initial_params=None):
     
     s_opts = {"max_iter": 200, 'step':0.1}
     
-    results_GRU = ParallelModelTraining(quality_model,data,initializations=10, BFR=False, 
-                      p_opts=None, s_opts=s_opts)
+    results_GRU = ParallelModelTraining(quality_model,data,initializations=20, BFR=False, 
+                      p_opts=None, s_opts=s_opts,mode='parallel',n_pool=10)
         
     pkl.dump(results_GRU,open('GRU_c'+str(dim_c)+'_1sub.pkl','wb'))
   
