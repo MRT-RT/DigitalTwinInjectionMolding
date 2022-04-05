c3ea82e9ae4097534fd1e4cb6bf479a94f8128da
SlothFox <alexander.rehmer@gmail.com>
2022-04-05 16:36:42 +0200

    process model

--- Experiments/ProcessModels/DynamicProcessModels/MLP_3sub.py
+++ Experiments/ProcessModels/DynamicProcessModels/MLP_3sub.py
@@ -22,20 +22,20 @@ from DIM.miscellaneous.PreProcessing import LoadDynamicData
 from DIM.models.model_structures import MLP
 from DIM.models.injection_molding import ProcessModel
 from DIM.optim.param_optim import parallel_mode
-from DIM.optim.param_optim import ParallelModelTraining
+from DIM.optim.param_optim import ModelTraining
 
 
 
 def Fit_MLP(dim_hidden,initial_params=None):
 
-    charges = list(range(1,275))    
+    charges = list(range(1,3))    
  
     split = 'process'
     mode = 'process'
 
-    # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-    path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     
     u_inj= ['v_inj_soll']
     u_press= ['p_inj_soll']
@@ -44,12 +44,14 @@ def Fit_MLP(dim_hidden,initial_params=None):
     u_lab = [u_inj,u_press,u_cool]
     y_lab = ['Q_Vol_ist','V_Screw_ist','p_wkz_ist','T_wkz_ist','p_inj_ist']
 
-    data,cycles_train_label,cycles_val_label,charge_train_label,charge_val_label = \
-    LoadDynamicData(path,charges,split,y_lab,u_lab,mode)
+    data_train, data_val = LoadDynamicData(path,charges,split,y_lab,u_lab,mode)
 
-    inj_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='inj')
-    press_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='press')
-    cool_model = MLP(dim_u=0,dim_out=5,dim_hidden=dim_hidden,name='cool')
+    inj_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,u_label=u_inj,
+                    y_label=y_lab,name='inj')
+    press_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,u_label=u_press,
+                    y_label=y_lab,name='press')
+    cool_model = MLP(dim_u=0,dim_out=5,dim_hidden=dim_hidden,u_label=u_cool,
+                    y_label=y_lab,name='cool')
 
     process_model = ProcessModel(subsystems=[inj_model,press_model,cool_model],
                                   name='p_model')   
@@ -57,9 +59,9 @@ def Fit_MLP(dim_hidden,initial_params=None):
 
     s_opts = {"max_iter": 2000, 'hessian_approximation':'limited-memory'}
 
-    results_MLP =  ParallelModelTraining(process_model,data,initializations=20, 
-                    BFR=False, p_opts=None, s_opts=s_opts,mode='parallel',
-                    n_pool=8)
+    results_MLP =  ModelTraining(process_model,data_train,data_val,
+                           initializations=20, BFR=False, p_opts=None, 
+                           s_opts=s_opts,mode='parallel', n_pool=8)
     
     pkl.dump(results_MLP,open('MLP_h'+str(dim_hidden)+'_3sub.pkl','wb'))
 
@@ -67,18 +69,16 @@ def Fit_MLP(dim_hidden,initial_params=None):
     return results_MLP
 
 
+h5 = Fit_MLP(dim_hidden=5)
 
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    # h1 = Fit_MLP(dim_hidden=1)
-    # h2 = Fit_MLP(dim_hidden=2)
-    h10 = Fit_MLP(dim_hidden=10)
-    h3 = Fit_MLP(dim_hidden=3)
-    h4 = Fit_MLP(dim_hidden=4)
-    h5 = Fit_MLP(dim_hidden=5)
-    h6 = Fit_MLP(dim_hidden=6)
-    h7 = Fit_MLP(dim_hidden=7)
-    h8 = Fit_MLP(dim_hidden=8)
-    h9 = Fit_MLP(dim_hidden=9)
-    h10 = Fit_MLP(dim_hidden=10)
-
+# if __name__ == '__main__':
+#     multiprocessing.freeze_support()
+#     h5 = Fit_MLP(dim_hidden=5)
+#     h10 = Fit_MLP(dim_hidden=10)
+#     h15 = Fit_MLP(dim_hidden=15)
+#     h20 = Fit_MLP(dim_hidden=20)
+#     h25 = Fit_MLP(dim_hidden=25)
+#     h30 = Fit_MLP(dim_hidden=30)
+#     h35 = Fit_MLP(dim_hidden=35)
+#     h40 = Fit_MLP(dim_hidden=40)
+#     h50 = Fit_MLP(dim_hidden=50)
