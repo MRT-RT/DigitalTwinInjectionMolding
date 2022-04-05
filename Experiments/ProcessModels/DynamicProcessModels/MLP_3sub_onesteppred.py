1c638b10c9aeebc728a7f59c808799efa06f663c
SlothFox <alexander.rehmer@gmail.com>
2022-04-05 13:58:46 +0200

    update

--- Experiments/ProcessModels/DynamicProcessModels/MLP_3sub_onesteppred.py
+++ Experiments/ProcessModels/DynamicProcessModels/MLP_3sub_onesteppred.py
@@ -6,9 +6,10 @@ Created on Mon Mar 14 10:17:29 2022
 @author: alexander
 """
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pickle as pkl
+from copy import deepcopy
 
 import multiprocessing
 
@@ -22,135 +23,87 @@ from DIM.miscellaneous.PreProcessing import LoadDynamicData
 from DIM.models.model_structures import MLP
 from DIM.models.injection_molding import ProcessModel
 from DIM.optim.param_optim import parallel_mode, series_parallel_mode
-from DIM.optim.param_optim import ModelTraining
+from DIM.optim.param_optim import ParallelModelTraining
 
-charges = list(range(1,3))    
+def Fit_MLP(dim_hidden,initial_params=None):
+
+    charges = list(range(1,275))    
  
-split = 'process'
-mode = 'process'
-
-# # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-# # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-
-u_inj= ['v_inj_soll']
-u_press= ['p_inj_soll']
-u_cool= []
-
-u_lab = [u_inj,u_press,u_cool]
-
-# u_lab = [u_inj,[],[]]
-y_lab = ['Q_Vol_ist','V_Screw_ist','p_wkz_ist','T_wkz_ist','p_inj_ist']
-
-data_train, data_val = LoadDynamicData(path,charges,split,y_lab,u_lab,mode)
-
-data_inj_train = data_train.copy()
-data_inj_val = data_val.copy()
-
-for i in range(len(data_inj_train['data'])):
-    t1 = data_inj_train['switch'][i][0]
-    data_inj_train['data'][i] = data_inj_train['data'][i].loc[0:t1]
-
-for i in range(len(data_inj_val['data'])):
-    t1 = data_inj_val['switch'][i][0]
-    data_inj_val['data'][i] = data_inj_val['data'][i].loc[0:t1]
-
-# data_cool = data.copy()
-
-# data_inj['u_train'] = [u[0] for u in data_inj['u_train']]
-# data_press['u_train'] = [u[0] for u in data_press['u_train']]
-# data_cool['u_train'] = [u[0] for u in data_cool['u_train']]
-
-# data_inj['u_val'] = [u[0] for u in data_inj['u_val']]
-# data_press['u_val'] = [u[0] for u in data_press['u_val']]
-# data_cool['u_val'] = [u[0] for u in data_cool['u_val']]
-
-
-
-# for u_key, y_key in zip(['u_train','u_val'],['y_train','y_val']):
-#     data_inj[u_key] = [el[0] for el in data_inj[u_key]] 
-#     data_inj[y_key] = [data_inj[y_key][i][0:data_inj[switch_train]
-                                          
-dim_hidden = 10
-
-inj_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,
-                u_label=u_inj,y_label=y_lab ,name='inj')
-# # press_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='press')
-# # cool_model = MLP(dim_u=0,dim_out=5,dim_hidden=dim_hidden,name='cool')
-
-# inj_training = ModelTraining(inj_model,data_inj_train,data_inj_val,initializations=5,mode='series')
-
-inj_training = pkl.load(open('inj_training.pkl','rb'))
-
-params_opt = inj_training.loc[1]['params_val']
-
-inj_model.Parameters = params_opt
-
-# Worst batch
-cyc = 1
-
-_,simulation = parallel_mode(inj_model,data_inj_val,params=None)
-
-# _,simulation = series_parallel_mode(inj_model,data_inj_val,params=None)
-
-
-plt.figure()
-plt.plot(data_inj_val['data'][cyc][y_lab])
-plt.plot(simulation[cyc])
-plt.legend()
-
-# def Fit_MLP(dim_hidden,initial_params=None):
-
-#     charges = list(range(1,3))    
- 
-#     split = 'process'
-#     mode = 'process'
-
-#     # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-#     path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
-#     # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    split = 'process'
+    mode = 'process'
     
-#     u_inj= ['v_inj_soll']
-#     u_press= ['p_inj_soll']
-#     u_cool= []
-
-#     u_lab = [u_inj,u_press,u_cool]
-#     y_lab = ['Q_Vol_ist','V_Screw_ist','p_wkz_ist','T_wkz_ist','p_inj_ist']
-
-#     data,cycles_train_label,cycles_val_label,charge_train_label,charge_val_label = \
-#     LoadDynamicData(path,charges,split,y_lab,u_lab,mode)
-
-#     inj_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='inj')
-#     press_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='press')
-#     cool_model = MLP(dim_u=0,dim_out=5,dim_hidden=dim_hidden,name='cool')
-
-#     process_model = ProcessModel(subsystems=[inj_model,press_model,cool_model],
-#                                   name='p_model')   
-
-
-#     s_opts = {"max_iter": 2000, 'hessian_approximation':'limited-memory'}
-
-#     results_MLP =  ModelTraining(process_model,data,initializations=2, 
-#                     BFR=False, p_opts=None, s_opts=s_opts,mode='series')
+    path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
+    # # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/'
     
-#     pkl.dump(results_MLP,open('MLP_h'+str(dim_hidden)+'_3sub.pkl','wb'))
+    u_inj= ['v_inj_soll']
+    u_press= ['p_inj_soll']
+    u_cool= []
+    
+    u_lab = [u_inj,u_press,u_cool]
+    
+    # u_lab = [u_inj,[],[]]
+    y_lab = ['Q_Vol_ist','V_Screw_ist','p_wkz_ist','T_wkz_ist','p_inj_ist']
+    
+    data_train, data_val = LoadDynamicData(path,charges,split,y_lab,u_lab,mode)
+    
+    data_inj_train = deepcopy(data_train)
+    data_inj_val = deepcopy(data_val)
+    
+    data_press_train = deepcopy(data_train)
+    data_press_val = deepcopy(data_val)
+    
+    data_cool_train = deepcopy(data_train)
+    data_cool_val = deepcopy(data_val)
+    
+    
+    for i in range(len(data_inj_train['data'])):
+        t1 = data_inj_train['switch'][i][0]
+        data_inj_train['data'][i] = data_inj_train['data'][i].loc[0:t1]
+        
+        t2 = data_press_train['switch'][i][1]
+        data_press_train['data'][i] = data_press_train['data'][i].loc[t1:t2]
+        
+        data_cool_train['data'][i] = data_cool_train['data'][i].loc[t2::]
+        
+    
+    for i in range(len(data_inj_val['data'])):
+        t1 = data_inj_val['switch'][i][0]
+        data_inj_val['data'][i] = data_inj_val['data'][i].loc[0:t1]
+        
+        t2 = data_inj_val['switch'][i][1]
+        data_press_val['data'][i] = data_press_val['data'][i].loc[t1:t2]
+        
+        data_cool_val['data'][i] = data_cool_val['data'][i].loc[t2::]
+
+    inj_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,u_label=u_inj,
+                    y_label=y_lab,name='inj')
+    
+    # press_model = MLP(dim_u=1,dim_out=5,dim_hidden=dim_hidden,name='press')
+    # cool_model = MLP(dim_u=0,dim_out=5,dim_hidden=dim_hidden,name='cool')
+
+    s_opts = {"max_iter": 2000, 'hessian_approximation':'limited-memory'}
+
+    results_inj =  ParallelModelTraining(inj_model,data_inj_train,data_inj_val,
+                           initializations=20,BFR=False, p_opts=None, 
+                           s_opts=s_opts,mode='series',n_pool=2)
+    
+    pkl.dump(results_inj,open('MLP_inj_h'+str(dim_hidden)+'.pkl','wb'))
 
 
-#     return results_MLP
+    return results_inj
 
 # h10 = Fit_MLP(dim_hidden=10)
 
-# if __name__ == '__main__':
-#     multiprocessing.freeze_support()
-#     # h1 = Fit_MLP(dim_hidden=1)
-#     # h2 = Fit_MLP(dim_hidden=2)
-#     h10 = Fit_MLP(dim_hidden=10)
-#     h3 = Fit_MLP(dim_hidden=3)
-#     h4 = Fit_MLP(dim_hidden=4)
-#     h5 = Fit_MLP(dim_hidden=5)
-#     h6 = Fit_MLP(dim_hidden=6)
-#     h7 = Fit_MLP(dim_hidden=7)
-#     h8 = Fit_MLP(dim_hidden=8)
-#     h9 = Fit_MLP(dim_hidden=9)
-#     h10 = Fit_MLP(dim_hidden=10)
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    h5 = Fit_MLP(dim_hidden=5)
+    h10 = Fit_MLP(dim_hidden=10)
+    h15 = Fit_MLP(dim_hidden=15)
+    h20 = Fit_MLP(dim_hidden=20)
+    h25 = Fit_MLP(dim_hidden=25)
+    h30 = Fit_MLP(dim_hidden=30)
+    h35 = Fit_MLP(dim_hidden=35)
+    h40 = Fit_MLP(dim_hidden=40)
+    h50 = Fit_MLP(dim_hidden=50)
 
