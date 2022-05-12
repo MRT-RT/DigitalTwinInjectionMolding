a509c4b71af06b61a43fba4e0c2ba814f1cc9917
SlothFox <alexander.rehmer@gmail.com>
2022-05-12 17:01:13 +0200

    Update Plot_Messprojektor_Data.py

--- Experiments/PlotLiveData/Plot_Messprojektor_Data.py
+++ Experiments/PlotLiveData/Plot_Messprojektor_Data.py
@@ -26,8 +26,8 @@ fig = plt.figure()
 
 colors = [plt.get_cmap('tab10')(i) for i in range(0,8)]
 
-D_axis = plt.axes([0.05, 0.05, 1, 0.4])
-D_mean_axis = plt.axes([0.05, 0.5, 1, 0.4])
+D_axis = plt.axes([0.05, 0.05, 0.9, 0.4])
+D_mean_axis = plt.axes([0.05, 0.5, 0.9, 0.4])
 
 D_line, = D_axis.plot([],[],'-o',alpha=0.8)
 
@@ -38,7 +38,9 @@ plt.show()
 t_mean = []
 mean = []
 
-for k in range(3,100):
+pause = 0.1
+
+for k in range(1,100000):
     
       
     # read csv
@@ -47,33 +49,43 @@ for k in range(3,100):
 
     
     # k = df_csv['laufenden Zhler'].max()
-    current_qual_meas = df_csv[df_csv['laufenden Zhler']==k][qual_label]
-    current_qual_meas = float(current_qual_meas.values[0].replace(',','.'))
-    
-    
-    if  k != df_plot.index.max():
-        df_plot.loc[k] = current_qual_meas
 
-        # move window 
-        t_min = max(0,k-window_with)
-        t_max = max(0,k-window_with)+window_with
-        
-        t = df_plot.index[t_min:t_max]
-        
-        D_line.set_data(t,df_plot[qual_label].loc[t])
-        
-        D_axis.set_xlim([t_min,t_max])
-        D_axis.set_ylim([df_plot[qual_label].min()-0.1,df_plot[qual_label].max()+0.1])
     
-    
-        if k % 10 == 0:
-            t_mean.append(k)
-            mean.append(df_plot[qual_label].loc[k-10:k].mean())
+    newdatum = False
+   
+    while newdatum == False:
+        
+        try:
+            current_qual_meas = df_csv[df_csv['laufenden Zhler']==k][qual_label]
+            current_qual_meas = float(current_qual_meas.values[0].replace(',','.'))
             
-            D_mean.set_data(t_mean,mean)
-            
-            D_mean_axis.set_xlim([t_min,t_max])
-            D_mean_axis.set_ylim([df_plot[qual_label].min()-0.1,df_plot[qual_label].max()+0.1])
+            df_plot.loc[k] = current_qual_meas
+            newdatum = True
+        except KeyError:
+            sleep(pause)
+        except IndexError:
+            sleep(pause)
+        
+    # move window 
+    t_min = max(0,k-window_with)
+    t_max = max(0,k-window_with)+window_with
     
-        plt.pause(0.5)
-        sleep(4)
+    t = df_plot.index[t_min:t_max]
+    
+    D_line.set_data(t,df_plot[qual_label].loc[t])
+    
+    D_axis.set_xlim([t_min,t_max])
+    D_axis.set_ylim([df_plot[qual_label].min()-0.1,df_plot[qual_label].max()+0.1])
+
+
+    if k % 10 == 0:
+        t_mean.append(k)
+        mean.append(df_plot[qual_label].loc[k-10:k].mean())
+        
+        D_mean.set_data(t_mean,mean)
+        
+        D_mean_axis.set_xlim([t_min,t_max])
+        D_mean_axis.set_ylim([df_plot[qual_label].min()-0.1,df_plot[qual_label].max()+0.1])
+
+    plt.pause(pause)
+    sleep(pause)
