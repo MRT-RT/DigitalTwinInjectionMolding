2d71097641f08133f3c0d5ffa524151f22b392fc
SlothFox <alexander.rehmer@gmail.com>
2022-05-17 12:01:52 +0200

    Versuchsplan merged

--- archiv/Convert_Data.py
+++ archiv/Convert_Data.py
@@ -16,8 +16,7 @@ sys.path.insert(0, "/home/alexander/GitHub/DigitalTwinInjectionMolding/")
 
 from DIM.miscellaneous.PreProcessing import hdf5_to_pd_dataframe_high_freq, add_csv_to_pd_dataframe
 
-csv_filename = 'Messdaten_Verschlusskappe_Versuchsplan_orig_Stgrsn.csv'
-weight_csv_filename = 'Gewicht_orig_Strgn.csv'
+csv_filename = 'Versuchsplan_Stgrn.csv'
 # path = '/home/alexander/Downloads/Versuchsplan/' # @work
 # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/HighFrequencyMeasurements/'
 
@@ -37,19 +36,18 @@ target_path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Stoergroe
 filenames = ['Versuchsplan_orig_Strgn.h5']
 
 
-# for filename in filenames:
-#     # Read h5 file
-#     file = h5py.File(path+filename,'r+')
+for filename in filenames:
+    # Read h5 file
+    file = h5py.File(path+filename,'r+')
     
-#     #convert and save as pd dataframe
-#     hdf5_to_pd_dataframe_high_freq(file,target_path)
+    #convert and save as pd dataframe
+    hdf5_to_pd_dataframe_high_freq(file,target_path)
     
 cycle_files = os.listdir(target_path)
 
 for cycle in cycle_files:
     
-    df = add_csv_to_pd_dataframe(target_path+cycle,path+csv_filename,
-                                 path+weight_csv_filename)
+    df = add_csv_to_pd_dataframe(target_path+cycle,path+csv_filename)
 
 # for i in range(1,251):
 #     c = pkl.load(open('cycle'+str(i)+'.pkl','rb'))
