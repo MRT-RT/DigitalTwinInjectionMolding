0f28674e6a986aa51e45975bf90615016fc2e6e5
SlothFox <alexander.rehmer@gmail.com>
2022-06-01 16:55:43 +0200

    Tutorials

--- Experiments/features_static_MLP/CompleteModel_feature_MLPs.py
+++ Experiments/features_static_MLP/CompleteModel_feature_MLPs.py
@@ -23,15 +23,14 @@ path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/nor
 
 # path = 'E:/GitHub/DigitalTwinInjectionMolding/data/Versuchsplan/normalized/'
 charges = list(range(1,275))
-split = 'all'
-
-
-
+split = 'process'
 
 u_label_p = ['Düsentemperatur', 'Werkzeugtemperatur', 'Einspritzgeschwindigkeit',
   'Umschaltpunkt', 'Nachdruckhöhe', 'Nachdruckzeit', 'Staudruck', 'Kühlzeit']
+
 u_label_q = ['T_wkz_0', 'T_wkz_max', 't_Twkz_max', 'T_wkz_int', 'p_wkz_max',
-'p_wkz_int', 'p_wkz_res', 't_pwkz_max']
+'p_wkz_int', 'p_wkz_res', 't_pwkz_max']                                         # = y_label_p
+
 y_label_q = ['Durchmesser_innen']
 
 data_train,data_val = LoadFeatureData(path,charges,split)
@@ -44,8 +43,8 @@ data_train,minmax = MinMaxScale(data_train,u_label_q+u_label_p+y_label_q)
 data_val,_ = MinMaxScale(data_val,u_label_q+u_label_p+y_label_q,minmax)
 
 
-# model_p = Static_MLP(dim_u=9, dim_out=3, dim_hidden=20,u_label=u_label_p,
-#                     y_label=u_label_q,name='proc', init_proc='xavier')
+model_p = Static_MLP(dim_u=8, dim_out=8, dim_hidden=10,u_label=u_label_p,
+                    y_label=u_label_q,name='proc', init_proc='xavier')
 
 model_q = Static_MLP(dim_u=8, dim_out=1, dim_hidden=4,u_label=u_label_q,
                     y_label=y_label_q,name='qual', init_proc='xavier')
@@ -54,35 +53,20 @@ model_q = Static_MLP(dim_u=8, dim_out=1, dim_hidden=4,u_label=u_label_q,
 # result_p = ModelTraining(model_p,data_train,data_val,initializations=5,
 #                           p_opts=None,s_opts=None,mode='static')
 
-result_q = ModelTraining(model_q,data_train,data_val,initializations=5,
-                          p_opts=None,s_opts=None,mode='static')
+# result_q = ModelTraining(model_q,data_train,data_val,initializations=5,
+#                           p_opts=None,s_opts=None,mode='static')
 
-# result_p = pkl.load(open('results_p_process.pkl','rb'))
-# result_q = pkl.load(open('results_q.pkl','rb'))
+result_p = pkl.load(open('results_p_process.pkl','rb'))
+result_q = pkl.load(open('results_q_feature.pkl','rb'))
 
-
-# model_p.Parameters = result_p.loc[0]['params_val']
+model_p.Parameters = result_p.loc[0]['params_val']
 model_q.Parameters = result_q.loc[0]['params_val']
 
-# _,prediction_q = static_mode(model_q,data_val)
-# _,prediction_p = static_mode(model_p,data_val)
+_,prediction_p = static_mode(model_p,data_val)
+_,prediction_q = static_mode(model_q,prediction_p)
 
 
-# print(BestFitRate(data_val[u_label_q].values, prediction_p[u_label_q].values))
-# print(BestFitRate(data_val[y_label_q].values, prediction_q[y_label_q].values))
-
-
-# fig, ax = plt.subplots(figsize=(20, 10))
-# sns.stripplot(x=data_val.index,y=data_val['p_wkz_max'],color='grey',alpha=.8,
-#               size=15,ax=ax)
-# sns.stripplot(x=prediction_p.index,y=prediction_p['p_wkz_max'],size=15,ax=ax)
-# ax.set_xlim([1,50]) 
-
-
-''' Evaluate whole model '''
-
-# _,prediction_p = static_mode(model_p,data_val)
-_,prediction_q = static_mode(model_q,data_val)
+print(BestFitRate(data_val[y_label_q].values, prediction_q[y_label_q].values))
 
 
 
@@ -93,7 +77,7 @@ _,prediction_q = static_mode(model_q,data_val)
 #               size=15,ax=ax)
 # ax.set_xlim([1,50]) 
 
-print(BestFitRate(data_val[y_label_q].values, prediction_q[y_label_q].values))
+
 
 
 
