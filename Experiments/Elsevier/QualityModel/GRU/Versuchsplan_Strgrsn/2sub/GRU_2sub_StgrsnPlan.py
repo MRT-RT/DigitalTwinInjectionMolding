0faa17496bab688fc4aaec8b64f78fe298510648
SlothFox <alexander.rehmer@gmail.com>
2022-06-02 16:13:51 +0200

    GRU 2sub started

--- Experiments/Elsevier/QualityModel/GRU/Versuchsplan_Strgrsn/2sub/GRU_2sub_StgrsnPlan.py
+++ Experiments/Elsevier/QualityModel/GRU/Versuchsplan_Strgrsn/2sub/GRU_2sub_StgrsnPlan.py
@@ -31,12 +31,12 @@ def Fit_GRU(dim_c,initial_params=None):
     split = 'all'
     mode='quality'
     
-    # path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Stoergroessen/20220504/Versuchsplan/normalized/'
-    path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Stoergroessen/20220504/Versuchsplan/normalized/'
+    path = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/data/Stoergroessen/20220504/Versuchsplan/normalized/'
+    # path = '/home/alexander/GitHub/DigitalTwinInjectionMolding/data/Stoergroessen/20220504/Versuchsplan/normalized/'
     
     u_inj= ['p_wkz_ist','T_wkz_ist']
     u_press= ['p_wkz_ist','T_wkz_ist']
-    # u_cool= ['p_wkz_ist','T_wkz_ist']
+
     
     u_lab = [u_inj,u_press]
     y_lab = ['Gewicht']
@@ -57,16 +57,13 @@ def Fit_GRU(dim_c,initial_params=None):
     
     press_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,
                       u_label=u_press,y_label=y_lab,dim_out=1,name='press')
-    
-    # cool_model = GRU(dim_u=2,dim_c=dim_c,dim_hidden=10,
-    #                  u_label=u_cool,y_label=y_lab,dim_out=1,name='cool')
+
     
     inj_model.InitialParameters = initial_params
     press_model.InitialParameters = initial_params 
-    # cool_model.InitialParameters = initial_params 
+
     
     press_model.InitialParameters ={'b_z_press':np.random.uniform(-10,-2,(dim_c,1))}
-    # cool_model.InitialParameters = {'b_z_cool':np.random.uniform(-10,-4,(dim_c,1))}
     
     quality_model = QualityModel(subsystems=[inj_model,press_model],
                                   name='q_model')
@@ -76,7 +73,7 @@ def Fit_GRU(dim_c,initial_params=None):
     
     # results_GRU = ParallelModelTraining(quality_model,data_train,data_val,
     #                         initializations=10, BFR=False, p_opts=None, 
-    #                         s_opts=s_opts,mode='parallel',n_pool=10)
+    #                         s_opts=s_opts,mode='parallel',n_pool=2)
 
     results_GRU = ModelTraining(quality_model,data_train,data_val,
                             initializations=10, BFR=False, p_opts=None, 
@@ -86,17 +83,6 @@ def Fit_GRU(dim_c,initial_params=None):
   
     return results_GRU  
 
-# c1_part = pkl.load(open('GRU_c1_3sub.pkl','rb'))
-# c1_all = Fit_GRU(dim_c=1,initial_params=c1_part.loc[4]['params_val'])
-
-# c2_part = pkl.load(open('GRU_c2_3sub.pkl','rb'))
-# c2_all = Fit_GRU(dim_c=2,initial_params=c2_part.loc[0]['params_val'])
-
-# c3_part = pkl.load(open('GRU_c3_3sub.pkl','rb'))
-# c3_all = Fit_GRU(dim_c=3,initial_params=c3_part.loc[0]['params_val'])
-
-# c4_part = pkl.load(open('GRU_c4_3sub.pkl','rb'))
-# c4_all = Fit_GRU(dim_c=4,initial_params=c4_part.loc[4]['params_val'])
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
