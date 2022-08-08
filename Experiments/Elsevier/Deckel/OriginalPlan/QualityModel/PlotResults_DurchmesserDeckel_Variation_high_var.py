0ff4392d0ff9024ded7d862654a7d1498c8a2932
SlothFox <alexander.rehmer@gmail.com>
2022-08-08 17:34:49 +0200

    MLP part2 experiments

---
+++ Experiments/Elsevier/Deckel/OriginalPlan/QualityModel/PlotResults_DurchmesserDeckel_Variation_high_var.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import pickle as pkl
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Result of models copy and pasted from Eval_..._.py
+
+poly_setpoints = pkl.load(open('./setpoint_models/setpoints/Poly_set_Durchmesser_high_var.pkl','rb'))
+poly_setpoints_x0 =    pkl.load(open('./setpoint_models/setpoints_initial_state/Poly_set_x0_Durchmesser_high_var.pkl','rb'))
+MLP_setpoints = pkl.load(open('./setpoint_models/setpoints/MLP_set_Durchmesser_high_var.pkl','rb'))
+MLP_setpoints_x0 = pkl.load(open('./setpoint_models/setpoints_initial_state/MLP_set_x0_Durchmesser_high_var.pkl','rb'))
+GRU = pkl.load(open('./GRU/Durchmesser_innen/GRU_3sub_Durchmesser_innen_high_var.pkl','rb'))
+
+res = pd.concat([poly_setpoints,poly_setpoints_x0,MLP_setpoints,
+                 MLP_setpoints_x0,GRU])
+
+poly_setpoints_best = poly_setpoints
+poly_setpoints_x0_best = poly_setpoints_x0
+MLP_setpoints_best = MLP_setpoints.sort_values('BFR',ascending=False).groupby('complexity').head(1)
+MLP_setpoints_x0_best = MLP_setpoints_x0.sort_values('BFR',ascending=False).groupby('complexity').head(1)
+GRU_best = GRU.sort_values('BFR',ascending=False).groupby('complexity').head(1)
+
+res_best = pd.concat([poly_setpoints_best,poly_setpoints_x0_best,
+                      MLP_setpoints_best,MLP_setpoints_x0_best,GRU_best])
+
+# res_best = poly_setpoints_best
+res_best.index = range(0,len(res_best))
+
+
+
+# Plot results static models
+plt.close('all')
+sns.set(font_scale = 0.7)
+color = sns.color_palette()
+
+fig,ax = plt.subplots(1,1)
+
+#Warum man hier den Index verschieben muss weiß kein Mensch
+sns.lineplot(x = res_best['complexity']-1,y=res_best['BFR'],palette=color[0:5],
+hue=res_best['model'],legend=False,ax=ax)
+
+stripplot = sns.boxplot(x = res['complexity'],y=res['BFR'],hue=res['model'],
+                          dodge=True,ax=ax,palette=color[0:5])
+
+legend = stripplot.get_legend()
+legend.remove()
+
+
+ax.set_xlabel('$n$')
+ax.set_ylabel('$\mathrm{BFR}$')
+
+fig.set_size_inches((8.4/2.54,8/2.54))
+# ax.set_xlim([0.8,10.2])
+ax.set_ylim([0.7,1])
+# ax.set_xticks(range(1,11))
+
+plt.tight_layout()
+plt.savefig('Results_Deckel_Durchmesser_high_var.png', bbox_inches='tight',dpi=600)
+
---
+++ Experiments/Elsevier/Deckel/OriginalPlan/QualityModel/setpoint_models/Gewicht/setpoints/setpoints_static_poly_filtered_by_variance.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 15:16:22 2022
+
+@author: LocalAdmin
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle as pkl
+import sys
+sys.path.insert(0, "/home/alexander/GitHub/DigitalTwinInjectionMolding/")
+sys.path.insert(0, 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/')
+sys.path.insert(0, 'E:/GitHub/DigitalTwinInjectionMolding/')
+
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from DIM.miscellaneous.PreProcessing import LoadFeatureData
+from sklearn.preprocessing import PolynomialFeatures
+
+path_sys = 'C:/Users/rehmer/Documents/GitHub/DigitalTwinInjectionMolding/'
+# path_sys = '/home/alexander/GitHub/DigitalTwinInjectionMolding/' 
+# path_sys = 'E:/GitHub/DigitalTwinInjectionMolding/'
+
+path = path_sys + 'data/Versuchsplan/normalized/'
+
+# Find charges with high / low variance
+plan = pkl.load(open(path+'Versuchsplan.pkl','rb'))
+
+charges_low_var = []
+charges_high_var = []
+
+
+for charge in list(range(1,275)):
+    plan_sub = plan.loc[plan['Charge']==charge]
+    
+    std = plan_sub['Gewicht'].std()
+    
+    if std > 0.035:
+        charges_high_var.append(charge)
+    else:
+        charges_low_var.append(charge)
+
+
+charges_train = list(range(1,275))
+split = 'all'
+del_outl = True
+
+targets = ['Gewicht']
+
+
+data_train,_  = LoadFeatureData(path,charges_train,split,del_outl)
+_,data_val  = LoadFeatureData(path,charges_train,split,del_outl)
+
+inputs = ['Düsentemperatur', 'Werkzeugtemperatur',
+       'Einspritzgeschwindigkeit', 'Umschaltpunkt', 'Nachdruckhöhe',
+       'Nachdruckzeit', 'Staudruck', 'Kühlzeit']
+
+data = []
+
+for i in range(1,11):
+    # Polynomial Model
+    poly = PolynomialFeatures(i)
+    X_poly_train = poly.fit_transform(data_train[inputs])
+    X_poly_val = poly.transform(data_val[inputs])
+    
+    
+    PolyModel = LinearRegression()
+    PolyModel.fit(X_poly_train,data_train[targets])
+    
+    BFR = PolyModel.score(X_poly_val,data_val[targets])
+
+    print('dim c:'+str(i)+' init:' + str(0) + ' BFR: ' + 
+          str(BFR))
+
+    data.append([BFR,'poly_set',i,targets[0],0])
+
+df = pd.DataFrame(data=data,columns=['BFR','model','complexity','target','init'])
+
+pkl.dump(df,open('Poly_set_Gewicht_all.pkl','wb'))
